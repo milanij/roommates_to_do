33720dfa3c44d1443a79d9a251211a5096d8b6a0
James Milani <james.milani@gmail.com>
2014-05-12 18:04:31 -0700

    first commit after rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RoommatesToDo::Application.config.secret_key_base = '8285522058257566ce4a9b87eab01b542ebc986d2dcefd5725706a80012f3153b5d50d993de76bb3ac8fd86b1b40dac74f5bdf41a5192828f48a8c0ef3239d13'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RoommatesToDo::Application.config.session_store :cookie_store, key: '_roommates_to_do_session'
